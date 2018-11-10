d0576132ac7ee13f1713503828a44bd5b37a5ce0
xltan <lidmuse@gmail.com>
2018-11-10 22:29:36 +0800

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -20,22 +20,21 @@ if status is-login
 end
 
 function fish_prompt --description 'Write out the prompt'
-	set -l color_cwd
     set -l suffix
     switch "$USER"
         case root
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
+            set suffix '# '
         case '*'
-            set color_cwd $fish_color_cwd
-            set suffix '>'
+            set suffix '> '
     end
 
-    echo -n -s (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
+	if [ $status -eq 0 ]
+		set suffix $suffix
+	else
+		set suffix (set_color $fish_color_error) $suffix
+	end
+
+    echo -n -s (prompt_pwd) $suffix
 end
 
 function mcd
