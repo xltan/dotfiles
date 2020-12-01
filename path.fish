81315389c9f034dfadcf02b830739348ddf62638
xltan <lidmuse@gmail.com>
2020-12-02 02:33:11 +0800

    update rcs

--- path.fish
+++ path.fish
@@ -1,1 +1,1 @@
-set -U fish_user_paths $HOME/.local/bin $HOME/.cargo/bin $GOPATH/bin /usr/local/bin /usr/local/sbin /usr/local/opt/llvm/bin 
+set -U fish_user_paths $HOME/.fzf/bin $HOME/.local/bin $HOME/.cargo/bin $GOPATH/bin /usr/local/bin /usr/local/sbin
