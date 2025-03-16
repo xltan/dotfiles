9819208a9389b2f0a251233b83e86ba5e4f61fd6
Sinon <lidmuse@gmail.com>
2025-03-16 22:31:30 +0800

    update configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,93 @@
-if status is-login; and not test -n "$TMUX"
-    tmux new -ADs scratch; and kill %self
-    echo "tmux failed to start; using plain fish shell"
+# if status is-login; and not test -n "$TMUX"
+#     tmux new -ADs scratch; and kill %self
+#     echo "tmux failed to start; using plain fish shell"
+# end
+if status is-interactive; and set -q SSH_CONNECTION
+    # Set TERM for SSH sessions
+    if test "$TERM" != "xterm-256color"
+        set -gx TERM xterm-256color
+    end
+
+    # Start tmux if not already inside
+    if not set -q TMUX
+        exec tmux new-session -A -s main
+    end
 end
 
 set -gx QUIET_DIRENV_DET 1
 set -gx EDITOR vi
 set -gx GOPATH $HOME/tools/go
 set -gx PYENV_ROOT $HOME/.pyenv
-set -gx HOMEBREW_ROOT /opt/homebrew
-set -gx BUN_INSTALL "$HOME/.bun"
 set -gx DENO_INSTALL "$HOME/.deno"
+
+set -l os_name (uname)
+set -l homebrew_paths
+set -l os_paths
+
+switch $os_name
+    case Darwin
+        set -gx HOMEBREW_ROOT /opt/homebrew
+        set homebrew_paths $HOMEBREW_ROOT/bin $HOMEBREW_ROOT/sbin
+        set -gx LANG en_US.UTF-8
+        set -gx LC_ALL en_US.UTF-8
+        set -gx CCACHE_DIR $HOME/Library/Caches/ccache
+        set os_paths /Applications/Android\ Studio.app/Contents/MacOS \
+            $HOME/.konan/kotlin-native-prebuilt-macos-aarch64-2.1.21/bin
+
+        for candidate in $HOME/Library/Android/sdk $HOME/Android/Sdk
+            if test -d $candidate
+                set -gx ANDROID_HOME $candidate
+                break
+            end
+        end
+        set -q ANDROID_HOME; or set -gx ANDROID_HOME $HOME/Library/Android/sdk
+
+        set -l macos_java_home /Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+        test -d $macos_java_home; and set -gx JAVA_HOME $macos_java_home
+
+    case Linux
+        set -q LANG; or set -gx LANG C.UTF-8
+        set -q LC_ALL; or set -gx LC_ALL $LANG
+        set -gx CCACHE_DIR $HOME/.cache/ccache
+
+        for candidate in $HOME/Android/Sdk $HOME/Library/Android/sdk
+            if test -d $candidate
+                set -gx ANDROID_HOME $candidate
+                break
+            end
+        end
+        set -q ANDROID_HOME; or set -gx ANDROID_HOME $HOME/Android/Sdk
+
+        switch (uname -m)
+            case arm64 aarch64
+                set os_paths $HOME/.konan/kotlin-native-prebuilt-linux-aarch64-2.1.21/bin
+            case '*'
+                set os_paths $HOME/.konan/kotlin-native-prebuilt-linux-x86_64-2.1.21/bin
+        end
+
+        if not set -q JAVA_HOME; and type -q javac; and type -q readlink
+            set -l javac_path (readlink -f (command -s javac))
+            set -gx JAVA_HOME (dirname (dirname $javac_path))
+        end
+end
+
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --strip-cwd-prefix --hidden --follow --exclude .swap --exclude .undo'
 set -gx FZF_CTRL_T_COMMAND "command find -L \$dir -type f 2> /dev/null | sed '1d; s#^\./##'"
 set -gx GPG_TTY (tty)
 set -gx PYTHONPATH .
 set -gx MANPAGER "vi +Man!"
+set -gx ANDROID_SDK_ROOT $ANDROID_HOME
+set -gx ANDROID_AVD_HOME $HOME/.android/avd
+# set -gx CC "/opt/homebrew/bin/ccache clang"
+# set -gx CXX "/opt/homebrew/bin/ccache clang++"
 
 fish_add_path /usr/local/sbin $HOME/.duolingo/bin $HOME/.local/bin $HOME/.cargo/bin $HOME/.deno/bin $GOPATH/bin \
-    $HOME/tools/flutter/bin $PYENV_ROOT/bin $HOMEBREW_ROOT/bin $HOMEBREW_ROOT/sbin \
-    $BUN_INSTALL/bin $DENO_INSTALL/bin $HOME/.npm-global/node_modules/.bin
+    $HOME/tools/flutter/bin $PYENV_ROOT/bin $homebrew_paths \
+    $DENO_INSTALL/bin $HOME/.npm-global/node_modules/.bin \
+    $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/emulator \
+    $os_paths $HOME/.bunv/bin $HOME/.bun/bin $HOME/.pub-cache/bin
+
+type -q brew; and eval "$(brew shellenv)"
 
 if not status is-interactive
     exit
@@ -68,11 +137,12 @@ set fish_greeting
 test -d .venv/bin; and source .venv/bin/activate.fish
 test -d .pyenv/bin; and source .pyenv/bin/activate.fish
 type -q zoxide; and source (zoxide init fish | psub)
-type -q fnm; and source (fnm env --use-on-cd | psub)
+type -q fnm; and source (fnm env --use-on-cd --shell fish | psub)
 type -q fzf; and source (fzf --fish | psub)
 
 type -q direnv; and source (direnv hook fish | psub)
 type -q pyenv; and source (pyenv init - | psub)
+type -q rbenv; and source (rbenv init - | psub)
 
 function mcd
     mkdir -pv "$argv"
@@ -81,36 +151,35 @@ end
 
 function npmig
     # function to install npm packages to .npm-global
+    if not type -q npm
+        echo "npm not found"
+        return 127
+    end
+
     cd $HOME/.npm-global
-    npm install --save "$argv"
+    and command npm install --save $argv
     cd -
 end
 
-abbr -a c cargo
+abbr -a c cursor
+abbr -a ca cargo
+abbr -a b bun
+abbr -a n npm
+abbr -a nr "npm run"
 abbr -a g git
 abbr -a icat "kitten icat"
 abbr -a lg lazygit
 abbr -a gs "gh copilot suggest"
 abbr -a ge "gh copilot explain"
-abbr -a nr "npm run"
+abbr -a cc claude
+abbr -a ccd claude --dangerously-skip-permissions
 
-alias v 'nvim --listen $HOME/.config/nvim/pipe/$(basename $PWD)'
 alias vim 'nvim --listen $HOME/.config/nvim/pipe/$(basename $PWD)'
 alias mux 'tmux new -ADs scratch'
 alias rgs 'rg --smart-case --hidden --no-heading --column'
 alias ... 'cd ../..'
 alias .... 'cd ../../..'
-alias assume "source (brew --prefix)/bin/assume.fish"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-# if test -f /Users/sinon/.pyenv/versions/anaconda3-2024.06-1/bin/conda
-#     eval /Users/sinon/.pyenv/versions/anaconda3-2024.06-1/bin/conda "shell.fish" "hook" $argv | source
-# else
-#     if test -f "/Users/sinon/.pyenv/versions/anaconda3-2024.06-1/etc/fish/conf.d/conda.fish"
-#         . "/Users/sinon/.pyenv/versions/anaconda3-2024.06-1/etc/fish/conf.d/conda.fish"
-#     else
-#         set -x PATH "/Users/sinon/.pyenv/versions/anaconda3-2024.06-1/bin" $PATH
-#     end
-# end
-# <<< conda initialize <<<
+if type -q brew
+    set -l assume_fish (brew --prefix)/bin/assume.fish
+    test -f $assume_fish; and alias assume "source $assume_fish"
+end
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,22 +1,25 @@
 -- Define the window filter
 -- Define a variable to store the previous application
-local KITTY = "kitty"
+local TERM = "cmux"
 
 local appKeyMappings = {
-	["^Code"] = { -- Another example app
-		{ { "cmd", "ctrl" }, "r", { "cmd", "ctrl", "shift" }, "r" },
-	},
-	["^OpenVPN Connect"] = { -- Another example app
-		{
-			{ "cmd" },
-			"w",
-			nil,
-			function()
-				local current_win = hs.window.frontmostWindow()
-				current_win:close()
-			end,
-		},
-	},
+  ["^Code"] = { -- Another example app
+    { { "cmd", "ctrl" }, "r", { "cmd", "ctrl", "shift" }, "r" },
+  },
+  ["^Cursor"] = { -- Another example app
+    { { "cmd", "ctrl" }, "r", { "cmd", "ctrl", "shift" }, "r" },
+  },
+  ["^OpenVPN Connect"] = { -- Another example app
+    {
+      { "cmd" },
+      "w",
+      nil,
+      function()
+        local current_win = hs.window.frontmostWindow()
+        current_win:close()
+      end,
+    },
+  },
 }
 
 -- Table to store dynamic hotkeys
@@ -24,52 +27,52 @@ local dynamicHotkeys = {}
 
 -- Function to find key mappings for an app based on regex
 local function getKeyMappingsForApp(appName)
-	for pattern, keyMappings in pairs(appKeyMappings) do
-		if string.match(appName, pattern) then
-			return keyMappings
-		end
-	end
-	return nil
+  for pattern, keyMappings in pairs(appKeyMappings) do
+    if string.match(appName, pattern) then
+      return keyMappings
+    end
+  end
+  return nil
 end
 
 -- Function to enable keybindings for a specific app
 local function enableKeyBindingsForApp(appName)
-	local keyMappings = getKeyMappingsForApp(appName)
-	if not keyMappings then
-		return
-	end
+  local keyMappings = getKeyMappingsForApp(appName)
+  if not keyMappings then
+    return
+  end
 
-	for _, shortcut in ipairs(keyMappings) do
-		local macMods, macKey, winMods, winKeyOrFunc = table.unpack(shortcut)
-		if type(winKeyOrFunc) == "string" then
-			-- Standard keystroke remapping
-			dynamicHotkeys[#dynamicHotkeys + 1] = hs.hotkey.bind(macMods, macKey, nil, function()
-				print(111, winMods[1], winMods[2], winKeyOrFunc)
-				hs.eventtap.keyStroke(winMods, winKeyOrFunc, 1)
-			end)
-		elseif type(winKeyOrFunc) == "function" then
-			-- Custom function mapping
-			dynamicHotkeys[#dynamicHotkeys + 1] = hs.hotkey.bind(macMods, macKey, nil, winKeyOrFunc)
-		end
-	end
+  for _, shortcut in ipairs(keyMappings) do
+    local macMods, macKey, winMods, winKeyOrFunc = table.unpack(shortcut)
+    if type(winKeyOrFunc) == "string" then
+      -- Standard keystroke remapping
+      dynamicHotkeys[#dynamicHotkeys + 1] = hs.hotkey.bind(macMods, macKey, nil, function()
+        print(111, winMods[1], winMods[2], winKeyOrFunc)
+        hs.eventtap.keyStroke(winMods, winKeyOrFunc, 1)
+      end)
+    elseif type(winKeyOrFunc) == "function" then
+      -- Custom function mapping
+      dynamicHotkeys[#dynamicHotkeys + 1] = hs.hotkey.bind(macMods, macKey, nil, winKeyOrFunc)
+    end
+  end
 end
 
 -- Function to disable all active keybindings
 local function disableAllKeyBindings()
-	for _, hotkey in ipairs(dynamicHotkeys) do
-		hotkey:delete()
-	end
-	dynamicHotkeys = {}
+  for _, hotkey in ipairs(dynamicHotkeys) do
+    hotkey:delete()
+  end
+  dynamicHotkeys = {}
 end
 
 -- App watcher to monitor active app
 local appWatcher = hs.application.watcher.new(function(appName, eventType, app)
-	if eventType == hs.application.watcher.activated then
-		disableAllKeyBindings()
-		enableKeyBindingsForApp(appName)
-	elseif eventType == hs.application.watcher.launched and appName == "kitty" then
-		app:mainWindow():centerOnScreen(nil, false, 0)
-	end
+  if eventType == hs.application.watcher.activated then
+    disableAllKeyBindings()
+    enableKeyBindingsForApp(appName)
+  elseif eventType == hs.application.watcher.launched and appName == TERM then
+    app:mainWindow():centerOnScreen(nil, false, 0)
+  end
 end)
 
 -- Start the app watcher
@@ -77,41 +80,41 @@ appWatcher:start()
 
 -- Expose a cleanup function for reloading
 local function stop()
-	disableAllKeyBindings()
-	appWatcher:stop()
+  disableAllKeyBindings()
+  appWatcher:stop()
 end
 
--- Switch kitty
+-- Switch Term
 hs.hotkey.bind(
-	{ "option" },
-	"space",
-	function() -- change your own hotkey combo here, available keys could be found here:https://www.hammerspoon.org/docs/hs.hotkey.html#bind
-		local app = hs.window.frontmostWindow():application()
-		if app and app:name() == KITTY then
-			app:hide()
-		else
-			hs.application.open(KITTY)
-		end
-	end
+  { "option" },
+  "space",
+  function() -- change your own hotkey combo here, available keys could be found here:https://www.hammerspoon.org/docs/hs.hotkey.html#bind
+    local app = hs.window.frontmostWindow():application()
+    if app and app:name() == TERM then
+      app:hide()
+    else
+      hs.application.open(TERM)
+    end
+  end
 )
 
 -- Kill all other windows
 hs.hotkey.bind(
-	{ "shift", "cmd" },
-	"q",
-	function() -- change your own hotkey combo here, available keys could be found here:https://www.hammerspoon.org/docs/hs.hotkey.html#bind
-		local current_win = hs.window.frontmostWindow()
-		local app = current_win:application()
-		-- closes all other windows
-		for _, win in ipairs(app:allWindows()) do
-			if win ~= current_win then
-				win:close()
-			end
-		end
-	end
+  { "shift", "cmd" },
+  "q",
+  function() -- change your own hotkey combo here, available keys could be found here:https://www.hammerspoon.org/docs/hs.hotkey.html#bind
+    local current_win = hs.window.frontmostWindow()
+    local app = current_win:application()
+    -- closes all other windows
+    for _, win in ipairs(app:allWindows()) do
+      if win ~= current_win then
+        win:close()
+      end
+    end
+  end
 )
 
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", function()
-	stop() -- Clean up the watcher and hotkeys
-	hs.reload()
+  stop() -- Clean up the watcher and hotkeys
+  hs.reload()
 end)
